00db2231007055aefc3c839198f7f0783bf70fc6
maaax7 <maxtuka@gmail.com>
2023-02-27 17:47:41 +0000

    alteracoes dos projetos criados para estudo de csharp

--- AprendendoCSharp/Hello-World/Program.cs
+++ AprendendoCSharp/Hello-World/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{ 
+    public static void Main (string[] args)
+    { 
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Toque enter para fechar...");
+        Console.ReadLine();
+    }
+}
 
